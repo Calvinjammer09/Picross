a45f9a4dcd1706fdb133ec0462757cbad4a6cd39
Calvinjammer09 <calvin.myer-most@sfbrightworks.org>
2024-06-19 13:37:57 -0700

    added moving while chiseling and a bunch of cases of moving while chiseling

--- Scenes/Map.gd
+++ Scenes/Map.gd
@@ -9,9 +9,14 @@ class Puzzle:
 	var BoardState : Array[String]
 	
 	var Tiles : Array
+	var ChiselMode : String
 	
 	var TileSize := 16
-
+	
+	var chisled = load("res://Sprites/Chisled.png")
+	var blank = load("res://Sprites/BlankTile.png")
+	var marked = load("res://Sprites/Marked.png")
+	
 	func puzzle_count(s:String) -> Array[int]:
 		'''Given a string representing a row/col, return the pattern'''
 		var result: Array[int] = []
@@ -53,7 +58,34 @@ class Puzzle:
 		for s in Bitmap:
 			print(s)
 	
-	
+	func chisel(tile:Sprite2D, x, y, input:String):
+		if ChiselMode == '':
+			if tile.texture == chisled and input != 'mark':
+				tile.set_texture(blank)
+				BoardState[x][y] = ' '
+				ChiselMode = 'blank'
+			elif tile.texture == blank and input != 'mark':
+				tile.set_texture(chisled)
+				BoardState[x][y] = 'X'
+				ChiselMode = 'chisel'
+			elif tile.texture != marked and input == 'mark':
+				tile.set_texture(marked)
+				ChiselMode = 'mark'
+			elif tile.texture == marked:
+				tile.set_texture(blank)
+				ChiselMode = 'blank'
+			
+		elif ChiselMode == 'blank':
+			if tile.texture != blank:
+				tile.set_texture(blank)
+				BoardState[x][y] == ' '
+				
+		elif ChiselMode == 'mark':
+			if tile.texture != marked:
+				tile.set_texture(marked)
+				
+		elif tile.texture != chisled:
+			tile.set_texture(chisled)
 
 @onready var first_move_timer = get_node('InitialMoveTimer')
 @onready var fast_move_timer = get_node('MoveTimer')
@@ -61,13 +93,16 @@ class Puzzle:
 @onready var lava_blob = get_node('LavaMap')
 @onready var cursor = get_node('Cursor')
 
-var chisled = load("res://Sprites/Chisled.png")
-var blank = load("res://Sprites/BlankTile.png")
-
 var tile_scale := 2
 var tile_size := 16 * tile_scale
 
+var last_tile_chiseled
+var last_tile_marked
+
 var puzzle
+
+var chisled = load("res://Sprites/Chisled.png")
+var blank = load("res://Sprites/BlankTile.png")
 
 var can_move := true
 var moving := false
@@ -85,8 +120,11 @@ func generate_map(image, puzzle_size:Vector2):
 
 	for y in image.get_height():
 		var line = ''
+		var first_board = ''
 		for x in image.get_width():
 			line += 'X' if image.get_pixel(x, y)[3] != 0 else ' '
+			first_board += ' '
+		new_puzzle.BoardState.append(first_board)
 		new_puzzle.Bitmap.append(line)
 
 	new_puzzle.print_bitmap()
@@ -109,28 +147,41 @@ func generate_map(image, puzzle_size:Vector2):
 		new_puzzle.Tiles.append(tile_row)
 
 	return new_puzzle
-			
-func chisel(tile:Sprite2D):
-	if tile.texture == chisled:
-		tile.set_texture(blank)
-		
-	else:
-		tile.set_texture(chisled)
 		
 
 func take_input():
 	var direction = Input.get_vector('left', 'right', 'up', 'down')
 	
-	var chisel = Input.is_action_just_pressed('break')
-	var _mark = Input.is_action_just_pressed('mark')
+	var chisel = Input.is_action_pressed('break')
+	var mark = Input.is_action_pressed('mark')
 	var got_escape = Input.is_action_pressed('quit')
 	
 	if got_escape:
 		get_tree().quit()
-		
-	if chisel:
+	
+	if chisel and mark:
 		var pos = (cursor.position + Vector2(1, 1)) / Vector2(tile_size, tile_size)
-		chisel(puzzle.Tiles[pos[1]][pos[0]])
+		puzzle.chisel(puzzle.Tiles[pos[1]][pos[0]], pos[1], pos[0], 'mark')
+	
+	elif chisel or mark:
+		var pos = (cursor.position + Vector2(1, 1)) / Vector2(tile_size, tile_size)
+		if chisel:
+			
+			if pos != last_tile_chiseled:
+				last_tile_chiseled = pos
+				puzzle.chisel(puzzle.Tiles[pos[1]][pos[0]], pos[1], pos[0], 'chisel')
+		else:
+			last_tile_chiseled = Vector2(-1, 0)
+			
+		if mark:
+			if pos != last_tile_marked:
+				last_tile_marked = pos
+				puzzle.chisel(puzzle.Tiles[pos[1]][pos[0]], pos[1], pos[0], 'mark')
+		else:
+			last_tile_chiseled = Vector2(-1, 0)
+			
+	else:
+		puzzle.ChiselMode = ''
 
 	var dir = direction[0] != 0 or direction[1] != 0
 
@@ -148,7 +199,6 @@ func take_input():
 
 	if can_move and dir:
 		cursor.position += direction.normalized() * tile_size
-		print((cursor.position + Vector2(1, 1)) / Vector2(tile_size, tile_size))
 		can_move = false
 
 func _physics_process(_delta):
