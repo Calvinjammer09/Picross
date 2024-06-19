f5c07f87b5b81d821ff0bc7b740e0cafa17a58ac
Calvinjammer09 <calvin.myer-most@sfbrightworks.org>
2024-06-19 11:09:45 -0700

    added wip puzzle drawing

--- Scenes/Map.gd
+++ Scenes/Map.gd
@@ -1,9 +1,19 @@
 extends TileMap
 
+
+
 class Puzzle:
+	var blank : Texture2D = load("res://Sprites/BlankTile.png")
+	var chisled : Texture2D = load("res://Sprites/Chisled.png")
+	
+	
 	var Name: String
 	var Bitmap: Array[String]
+	var BoardSize : Vector2
+	var CompletedTiles : Array
 	
+	var TileSize := 16
+
 	func puzzle_count(s:String) -> Array[int]:
 		'''Given a string representing a row/col, return the pattern'''
 		var result: Array[int] = []
@@ -40,11 +50,12 @@ class Puzzle:
 				s += row[i]
 			result.append( puzzle_count(s) )
 		return result
+				
 	
 
-@onready var first_move_timer = get_node("InitialMoveTimer")
-@onready var fast_move_timer = get_node("MoveTimer")
-@onready var test_sprite = get_node("Pickaxe")
+@onready var first_move_timer = get_node('InitialMoveTimer')
+@onready var fast_move_timer = get_node('MoveTimer')
+@onready var test_sprite = get_node('Pickaxe')
 @onready var lava_blob = get_node('LavaMap')
 
 var tile_size := 16
@@ -55,31 +66,17 @@ var last_input : Array = [0, 0, 0, 0]
 var last_movement_x := 0
 var last_movement_y := 0
 
-var current_image_size := Vector2 (20, 20)
-
 func _ready():
-	puzzle_demo()
-	generate_map(lava_blob.texture.get_image())
+	generate_map(lava_blob.texture.get_image(), Vector2(20, 20))
 
 func print_bitmap(p):
 	for s in p.Bitmap:
 		print(s)
 
-func puzzle_demo():
-	var p := Puzzle.new()
-	p.Name = "easy puzzle"
-	p.Bitmap = [
-		" X ", 
-		"XXX",
-		"X X"]
-	
-	print_bitmap(p)
-	print("Rows:", p.puzzle_rows())
-	print("Cols:", p.puzzle_cols())
-
-
-func generate_map(image):
+func generate_map(image, puzzle_size:Vector2):
 	var new_puzzle = Puzzle.new()
+	
+	new_puzzle.BoardSize = puzzle_size
 
 	for y in image.get_width():
 		var line = ''
@@ -90,17 +87,25 @@ func generate_map(image):
 	print_bitmap(new_puzzle)
 	print("Rows:", new_puzzle.puzzle_rows())
 	print("Cols:", new_puzzle.puzzle_cols())
+	
+	for x in range(new_puzzle.BoardSize[0]):
+		for y in range(new_puzzle.BoardSize[1]):
+			var new_brick = Sprite2D.new()
+			new_brick.position = Vector2(x * tile_size, y * tile_size)
+			new_brick.set_texture(new_puzzle.blank)
 
+			add_child(new_brick)
+			
 func take_input():
 	var direction = Input.get_vector('left', 'right', 'up', 'down')
-	var got_escape = Input.is_action_pressed('quit')
-	if got_escape:
-		get_tree().quit()
+	
 
 	var _break_check = Input.is_action_pressed('break')
 	var _mark = Input.is_action_pressed('mark')
-
-	var input_list = [1 if direction[0] < 0 else 0, 1 if direction[0] > 0 else 0, 1 if direction[1] < 0 else 0, 1 if direction[1] > 0 else 1]
+	var got_escape = Input.is_action_pressed('quit')
+	
+	if got_escape:
+		get_tree().quit()
 
 	var dir = direction[0] != 0 or direction[1] != 0
 
