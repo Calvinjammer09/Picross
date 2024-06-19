049c676967ea7853c65d1c3528c36d3d9dee2d2a
Calvinjammer09 <calvin.myer-most@sfbrightworks.org>
2024-06-19 12:16:14 -0700

    added cursor and started with tile functionality

--- Scenes/Map.gd
+++ Scenes/Map.gd
@@ -3,14 +3,10 @@ extends TileMap
 
 
 class Puzzle:
-	var blank : Texture2D = load("res://Sprites/BlankTile.png")
-	var chisled : Texture2D = load("res://Sprites/Chisled.png")
-	
-	
 	var Name: String
 	var Bitmap: Array[String]
 	var BoardSize : Vector2
-	var CompletedTiles : Array
+	var BoardState : Array[String]
 	
 	var TileSize := 16
 
@@ -51,14 +47,23 @@ class Puzzle:
 			result.append( puzzle_count(s) )
 		return result
 				
+	func print_bitmap():
+		for s in Bitmap:
+			print(s)
+	
 	
 
 @onready var first_move_timer = get_node('InitialMoveTimer')
 @onready var fast_move_timer = get_node('MoveTimer')
 @onready var test_sprite = get_node('Pickaxe')
 @onready var lava_blob = get_node('LavaMap')
+@onready var cursor = get_node('Cursor')
 
-var tile_size := 16
+var chisled = load("res://Sprites/Chisled.png")
+var blank = load("res://Sprites/BlankTile.png")
+
+var tile_scale := 2
+var tile_size := 16 * tile_scale
 
 var can_move := true
 var moving := false
@@ -67,11 +72,7 @@ var last_movement_x := 0
 var last_movement_y := 0
 
 func _ready():
-	generate_map(lava_blob.texture.get_image(), Vector2(20, 20))
-
-func print_bitmap(p):
-	for s in p.Bitmap:
-		print(s)
+	var puzzle = generate_map(lava_blob.texture.get_image(), Vector2(20, 20))
 
 func generate_map(image, puzzle_size:Vector2):
 	var new_puzzle = Puzzle.new()
@@ -83,24 +84,35 @@ func generate_map(image, puzzle_size:Vector2):
 		for x in image.get_height():
 			line += 'X' if image.get_pixel(x, y)[3] != 0 else ' '
 		new_puzzle.Bitmap.append(line)
-		
-	print_bitmap(new_puzzle)
+
+	new_puzzle.print_bitmap()
 	print("Rows:", new_puzzle.puzzle_rows())
 	print("Cols:", new_puzzle.puzzle_cols())
 	
 	for x in range(new_puzzle.BoardSize[0]):
 		for y in range(new_puzzle.BoardSize[1]):
 			var new_brick = Sprite2D.new()
+			new_brick.centered = false
 			new_brick.position = Vector2(x * tile_size, y * tile_size)
-			new_brick.set_texture(new_puzzle.blank)
+			new_brick.scale = Vector2(tile_scale, tile_scale)
+
+			new_brick.set_texture(blank)
 
 			add_child(new_brick)
+	
+	return new_puzzle
 			
+func chisel(tile:Sprite2D, puzzle:Puzzle):
+	if tile.texture == chisled:
+		tile.set_texture(blank)
+	else:
+		tile.set_texture(chisled)
+		
+
 func take_input():
 	var direction = Input.get_vector('left', 'right', 'up', 'down')
 	
-
-	var _break_check = Input.is_action_pressed('break')
+	var _chisel = Input.is_action_pressed('break')
 	var _mark = Input.is_action_pressed('mark')
 	var got_escape = Input.is_action_pressed('quit')
 	
@@ -122,17 +134,15 @@ func take_input():
 		can_move = true
 
 	if can_move and dir:
-		test_sprite.position += direction.normalized() * tile_size
+		cursor.position += direction.normalized() * tile_size
+		print((cursor.position + Vector2(1, 1)) / Vector2(tile_size, tile_size))
 		can_move = false
-		print('moving')
 
 func _physics_process(_delta):
 	take_input()
 	
 func _on_initial_move_timer_timeout():
-	print('first timer check')
 	can_move = true
 
 func _on_move_timer_timeout():
-	print('second timer check')
 	can_move = true
